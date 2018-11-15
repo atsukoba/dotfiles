149a2aef260c0ce69d7f6473da23f36930af9e15
Atsuya Kobayashi <wgp_atsuya_g2d3y@yahoo.co.jp>
2018-11-16 05:09:03 +0900

    Create jupyterlab_settings.sh

---
+++ jupyterlab_settings.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# AtsuyaKobayashi
+#
+## Requirements
+# Python(Anaconda), nodejs, npm, 
+
+## install of jupyter lab itself(if needed)
+conda install -c conda-forge jupyterlab
+
+## install of labextention
+jupyter labextension list
+
+## install ipywidgets 
+conda install -c conda-forge ipywidgets
+
+## install ipywidgets jupyterlab
+# (if needed)
+# jupyter lab clean
+
+jupyter labextension install @jupyter-widgets/jupyterlab-manager
+
+## install of git extention
+jupyter labextension install @jupyterlab/git
+pip install jupyterlab-git
+jupyter serverextension enable --py jupyterlab_git
